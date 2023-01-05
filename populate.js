26c05f0af03c74669cc8cdbd6cbb9441ce09d8e4
maria-ys <ys.mariayounas@gmail.com>
2023-01-05 15:03:50 +0500

    populated data

---
+++ populate.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const connectDB = require("./db/connect");
+
+const mongoose = require("mongoose");
+const Product = require("./models/product");
+
+const productData = require("./products.json");
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    await Product.deleteMany();
+    await Product.create(productData);
+    console.log("Success!!");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
