2b484ec5f500c66934e68d9a0a78a0848fe15795
maria-ys <ys.mariayounas@gmail.com>
2023-01-07 09:41:43 +0500

    search for a specific name, company or featured value

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,26 @@
+const Product = require('../models/product');
+
 const getAllProducts = async (req, res) => {
   res.status(200).json({ msg: "all products" });
 };
 
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "all products testing" });
+  const {featured, company, name} = req.query;
+  const queryObject = {};
+
+  if(featured){
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  if(company){
+    queryObject.company = company;
+  }
+  if(name){
+    queryObject.name = {$regex: name, $options: 'i'};
+  }
+
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+  res.status(200).json(products);
 };
 
 module.exports = {
