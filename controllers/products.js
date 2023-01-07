660a91771222e92da56a561068a88b144b6711de
maria-ys <ys.mariayounas@gmail.com>
2023-01-07 09:58:34 +0500

    sorting, selecting fields and paging

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,7 @@
 const Product = require('../models/product');
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "all products" });
-};
-
-const getAllProductsStatic = async (req, res) => {
-  const {featured, company, name} = req.query;
+  const {featured, company, name, sort, fields} = req.query;
   const queryObject = {};
 
   if(featured){
@@ -18,9 +14,32 @@ const getAllProductsStatic = async (req, res) => {
     queryObject.name = {$regex: name, $options: 'i'};
   }
 
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
-  res.status(200).json(products);
+  var result = Product.find(queryObject);
+
+  // sorting 
+  if(sort){
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  }
+
+  // fields 
+  if(fields){
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
+  }
+
+  // paging & skipping
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+
+  const products = await result;
+  res.status(200).json({products, nbHits: products.length});
+};
+
+const getAllProductsStatic = async (req, res) => {
+  res.status(200).json({ products: await Product.find({}) });
 };
 
 module.exports = {
